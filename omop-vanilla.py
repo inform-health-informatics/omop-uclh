110e454768d0c09343b48aba15a4a081b14c85db
Steve Harris <doc@steveharris.me>
2018-10-26 00:18:38 +0100

    completed load from start to finish but with errors

--- omop-vanilla.py
+++ omop-vanilla.py
@@ -1,4 +1,6 @@
-from utils.utils import gen_connection_string
+# reload(utils.utils)
+import re
+from utils.utils import gen_connection_string, build_from_vocab_csv, clean_start
 import psycopg2
 
 # - [ ] @TODO: (2018-10-23) set these up as environment variables
@@ -8,48 +10,84 @@ connection_dict = {'user': 'uclh',
                    'password': 'uclh',
                    'database': 'OMOPUCLH'
                    }
-
 DSN = gen_connection_string(connection_dict, engine='psycopg2')
+force = True
 
-def clean_start():
-    """Drop all existing tables and data"""
-    # https://stackoverflow.com/questions/3327312/drop-all-tables-in-postgresql
-    SQL = """
-    DO $$ DECLARE
-    r RECORD;
-    BEGIN
-        -- if the schema you operate on is not "current", you will want to
-        -- replace current_schema() in query with 'schematodeletetablesfrom'
-        -- *and* update the generate 'DROP...' accordingly.
-        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-            EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-        END LOOP;
-    END $$;
-    """
-    with psycopg2.connect(DSN) as conn:
-        with conn.cursor() as curs:
-            rp = curs.execute(SQL)
-
-clean_start()
+if force:
+    clean_start(DSN, force)
 
 # - [ ] @TODO: (2018-10-25) convert to git submodule or pull specific commit
 # The order is important
 CDM_files_path = 'CommonDataModel/PostgreSQL/'
-CDM_files_names = [
-'OMOP CDM postgresql ddl.txt',
-'OMOP CDM postgresql indexes.txt',
-'OMOP CDM postgresql constraints.txt',
-]
+CDM_files_names = """
+OMOP CDM postgresql ddl.txt
+OMOP CDM postgresql indexes.txt
+OMOP CDM postgresql constraints.txt
+""".splitlines()[1:]
 
-for file_name in CDM_files_names:
-    fp = CDM_files_path + file_name
+VOCAB_files_path = 'vocab/'
+VOCAB_file_names = """
+CONCEPT.csv
+CONCEPT_ANCESTOR.csv
+CONCEPT_CLASS.csv
+CONCEPT_RELATIONSHIP.csv
+CONCEPT_SYNONYM.csv
+DOMAIN.csv
+DRUG_STRENGTH.csv
+RELATIONSHIP.csv
+VOCABULARY.csv
+""".splitlines()[1:]  # drop empty first line from docstring
+
+
+# Create tables and populate with vocab BEFORE applying indices and constraints
+# =============================================================================
+# Create empty tables
+file_name = CDM_files_names[0]
+fp = CDM_files_path + file_name
+with open(fp, 'r') as f:
+    SQL = f.read()
+    with psycopg2.connect(DSN) as conn:
+        with conn.cursor() as curs:
+            rp = curs.execute(SQL)
+
+# Load vocab
+for file_name in VOCAB_file_names:
+    fp = VOCAB_files_path + file_name
+    vocab_table = file_name[:-4]  # drop '.csv'
+    print('--- Loading {} into {}'.format(fp, vocab_table))
+    try:
+        build_from_vocab_csv(DSN, vocab_table.lower(), fp, force)
+    except psycopg2.DataError as e:
+        print('!!! FAILED loading {} into {}, {}'.format(fp, vocab_table, e))
+
+
+# - [ ] @TODO: (2018-10-25) fails for drug_strength
+# build_from_vocab_csv(DSN, 'drug_strength', 'vocab/DRUG_STRENGTH.csv', force)
+
+# Create indices and constraints (remaining 2 files)
+# Do this line by line to manage memory and disk space better?
+def extract_SQL_from_file(fp):
+    reSQL = re.compile(r'^\w.*;')
     with open(fp, 'r') as f:
-        SQL = f.read()
-        with psycopg2.connect(DSN) as conn:
-            with conn.cursor() as curs:
-                rp = curs.execute(SQL)
+        lines = f.read()
+    return [i for i in lines.splitlines() if re.match(reSQL, i)]
 
 
+def execute_SQL(DSN, SQL):
+    print('--- Executing: {} '.format(SQL))
+    with psycopg2.connect(DSN) as conn:
+        with conn.cursor() as curs:
+            try:
+                curs.execute(SQL)
+            except psycopg2.ProgrammingError as e:
+                print('!!! ProgrammingError executing: {}, {}'.format(SQL, e))
+            except psycopg2.OperationalError as e:
+                print('!!! OperationalError executing: {}, {}'.format(SQL, e))
+            except psycopg2.IntegrityError as e:
+                print('!!! IntegrityError executing: {}, {}'.format(SQL, e))
 
 
-
+for file_name in CDM_files_names[1:]:
+    fp = CDM_files_path + file_name
+    SQLs = extract_SQL_from_file(fp)
+    [execute_SQL(DSN, SQL) for SQL in SQLs]
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 # General utilities and helpers
+import csv
+import psycopg2
 
 
 def gen_connection_string(d, engine='postgresql'):
@@ -18,3 +20,42 @@ def gen_connection_string(d, engine='postgresql'):
             return 'host={host} dbname={database} user={user} password={password} port={port}'.format(**d)
         else:
             return 'host={host} dbname={database} user={user} port={port}'.format(**d)
+
+
+def build_from_vocab_csv(DSN, vocab_table, vocab_csv, force=False):
+    """Given a Table object then populate that table from a CSV file
+    using psycopg2 copy_from as a much more efficient bulk insert tool"""
+
+    if force:
+        SQL = "TRUNCATE {};".format(vocab_table)
+        with psycopg2.connect(DSN) as conn:
+            with conn.cursor() as curs:
+                rp = curs.execute(SQL)
+
+    with open(vocab_csv, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # skip the header row
+        with psycopg2.connect(DSN) as conn:
+            with conn.cursor() as curs:
+                curs.copy_from(f, vocab_table, sep='\t')
+
+
+def clean_start(DSN, force):
+    """Drop all existing tables and data from a schema"""
+    # https://stackoverflow.com/questions/3327312/drop-all-tables-in-postgresql
+    SQL = """
+    DO $$ DECLARE
+    r RECORD;
+    BEGIN
+        -- if the schema you operate on is not "current", you will want to
+        -- replace current_schema() in query with 'schematodeletetablesfrom'
+        -- *and* update the generate 'DROP...' accordingly.
+        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+            EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+        END LOOP;
+    END $$;
+    """
+    if force:
+        with psycopg2.connect(DSN) as conn:
+            with conn.cursor() as curs:
+                rp = curs.execute(SQL)
