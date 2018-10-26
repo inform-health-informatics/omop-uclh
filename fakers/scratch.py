b829fe6ddace8b3b2b07ff02586ca606a03d86f4
Steve Harris <doc@steveharris.me>
2018-10-26 15:31:03 +0100

    faker now creates spells with appropriate timestamps

--- fakers/scratch.py
+++ fakers/scratch.py
@@ -3,6 +3,8 @@
 A factory function and a series of classes that can be called to generate fake
 data
 """
+import numpy as np
+import datetime as dt
 
 
 def fake_it(seed=42, n_subspells=1):
@@ -21,10 +23,11 @@ def fake_it(seed=42, n_subspells=1):
         seed {number} -- [description] (default: {42})
         n_subspells {number} -- [description] (default: {1})
     """
+    np.random.seed(seed)
 
     patient = Patient()
-    spell = Spell()
-    subspells = (Subspell(i) for i in range(n_subspells))
+    spell = Spell(patient)
+    subspells = (Subspell(spell, patient, i) for i in range(n_subspells))
 
     # - [ ] @TODO: (2018-10-26) enhanced data for OMOP plus
     # location (care_site) at ward level
@@ -43,11 +46,47 @@ class Faker():
 
 
 class Patient():
-    pass
+    """Group and define patient level attributes
+
+    e.g.
+    - name
+    - date of birth
+    - hospital number (modelled to look like UCLH)
+    - NHS number
+    These need to be defined before the spell since you can't be born during an
+    admission!
+    """
+
+    def __init__(self):
+        self.id_mrn = '4{:07}'.format((np.random.randint(10000000)))
+        self.id_nhs = '{:03} {:04} {:04}'.format(
+            np.random.randint(1000),
+            np.random.randint(10000),
+            np.random.randint(10000))
+        # - [ ] @TODO: (2018-10-26) implement dob paying attention that age will
+        #   affect spell dates since you can't be admitted before you are born
+        #   or after you die!
+
+    def __str__(self):
+        # str is the pretty version / repr is the dev version
+        return 'UCLH patient MRN {}'.format(self.id_mrn)
 
 
 class Spell():
-    pass
+    """A period of care delivered by a single care provider
+
+    This is equivalent to an OMOP visit
+
+    Variables:
+        pass {[type]} -- [description]
+    """
+
+    def __init__(self, patient):
+
+        los_days = min(np.random.lognormal(1.5,1.), 365)
+        self.los_hours = dt.timedelta(hours=los_days*24)
+        self.start = dt.datetime.utcnow() - dt.timedelta(hours = np.random.uniform(0.0, 24*(365 - los_days))) - self.los_hours
+        self.stop = self.start + self.los_hours
 
 
 class Subspell(Spell):
@@ -56,5 +95,23 @@ class Subspell(Spell):
     pass
 
 
+# - [ ] @TODO: (2018-10-26) implement tests for this class
+# testing
 mrjones = fake_it(seed=42, n_subspells=1)
 print(mrjones)
+print(mrjones.patient)
+print(mrjones.patient.id_nhs)
+print(mrjones.spell.start)
+print(mrjones.spell.los_hours)
+print(mrjones.spell.stop)
+
+for i in range(10):
+    # switch seed off for randomness
+    mrsjones = fake_it(seed=None, n_subspells=1)
+    print(mrsjones)
+    print(mrsjones.patient)
+    print(mrsjones.patient.id_nhs)
+    print(mrsjones.spell.start)
+    print(mrsjones.spell.los_hours)
+    print(mrsjones.spell.stop)
+
