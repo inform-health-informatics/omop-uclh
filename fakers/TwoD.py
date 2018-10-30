d5e8a1db77e173f5c973c0894331484466ae1894
Steve Harris <doc@steveharris.me>
2018-10-30 23:20:34 +0000

    fixed relative import

--- fakers/TwoD.py
+++ fakers/TwoD.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from abc import ABC, abstractmethod, abstractproperty
-from fakers.faker import ConceptKeys  # named tuple of ids and names
+from .faker import ConceptKeys  # named tuple of ids and names
 
 
 class TwoD(ABC):
