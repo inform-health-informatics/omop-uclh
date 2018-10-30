05386ee9a58de32e7fec2705fb30d3f0847f4a75
Steve Harris <doc@steveharris.me>
2018-10-30 00:14:47 +0000

    switched to ABC implementation

--- fakers/TwoD.py
+++ fakers/TwoD.py
@@ -5,40 +5,71 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+from abc import ABC, abstractmethod, abstractproperty
+from fakers.faker import ConceptKeys  # named tuple of ids and names
 
 
-class TwoD():
-    # generic methods such as creating time series etc
+class TwoD(ABC):
+    # simulate 2d data
 
-    def __init__(self, spell, cadence=None):
+    def __init__(self, ts, colname=None):
 
-        self.spell = spell
+        self.ts = ts # 2d timeseries
+        self.colname = colname
+        self.colname = self._gen_colname() # to label data
 
-    def gen_time_series(self, cadence=None):
-        # return times by natural cadence
-        # - daily bloods
-        # - regular ward obs
-        # - close monitoring in critical care
-        # - [ ] @TODO: (2018-10-26) implement missingness
-        # - [ ] @TODO: (2018-10-26) implemnent jitter so times don't line up
+    @abstractproperty
+    def conceptkeys(self):
+        """Should return a ConceptKeys tuple for every class
 
-        # default cadence
-        if cadence is None:
-            cadence = '1D'
+        concept_id should be mandated but shortname and FSN should be optional
 
-        ts = pd.date_range(self.spell.start, self.spell.stop, freq=cadence)
+        Decorators:
+            abstractproperty
+        """
+        pass
 
-        return ts
-
+    def _gen_colname(self):
+        """Generate a suitable column name for the time series
+        """
+        if self.colname is not None:
+            return self.colname
+        elif self.conceptkeys.shortName is not None:
+            return self.conceptkeys.shortName
+        else:
+            return str(self.conceptkeys.concept_id)
 
 
 class Lactate(TwoD):
 
-    # - [ ] @TODO: (2018-10-26) implement ABC; require concept mapping
+    @property
+    def conceptkeys(self):
+        return ConceptKeys(3047181, 'lactate', 'Lactate [Moles/volume] in Blood')
 
-    # calls parent's init by default so you get time series for free
+    # - [ ] @TODO: (2018-10-30) use mixins for these simulations
     def simulate(self, cadence=None):
-        ts = self.gen_time_series(cadence)
-        vals =  [np.random.lognormal() for i in ts]
-        return pd.DataFrame.from_dict({'timestamp': ts, 'lacate': vals})
+        ts = self.ts
+        vals = [np.random.lognormal() for i in ts]
+        return pd.DataFrame.from_dict({'timestamp': ts, self.colname: vals})
 
+
+class HeartRate(TwoD):
+
+    @property
+    def conceptkeys(self):
+        return ConceptKeys(4239408, 'hrate', 'Heart rate (observable entity)')
+
+    def simulate(self, cadence=None):
+        ts = self.ts
+        vals = [np.random.normal(90, 15) for i in ts]
+        return pd.DataFrame.from_dict({'timestamp': ts, self.colname: vals})
+
+
+
+# - [ ] @TODO: (2018-10-30) convert to a test
+# ts = pd.date_range('2018-10-29', '2018-10-30', freq='4H')
+# hr.conceptkeys.shortName
+# hr = HeartRate(ts)
+# hr.simulate()
+# lac = Lactate(ts)
+# lac.simulate()
