09d9c69a18d45040de1c4d4b2850fb35945152cc
Steve Harris <doc@steveharris.me>
2018-11-01 21:42:42 +0000

    function to parse table DDL to column definitions

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,45 @@
 # General utilities and helpers
 import csv
 import psycopg2
+from typing import List
+
+
+def parse_table_ddl(ddl: str, dialect: str ='PostgreSQL') -> List:
+    """Parse SQL CREATE TABLE syntax into a list of tuples
+
+    Arguments:
+        ddl {str} -- [description]
+
+    Keyword Arguments:
+        dialect {str} -- [description] (default: {'PostgreSQL'})
+    """
+    from collections import namedtuple
+
+    data_types = tuple(
+        "INTEGER DATE VARCHAR TIMESTAMP TIME NUMERIC TEXT".lower().split())
+
+    ColumnDefinition = namedtuple('ColumnDefinition', [
+        'name',   # column name
+        'type',   # data type e.g. INTEGER, VARCHAR etc.
+        'null'    # NULL or NOT NULL
+    ])
+
+    column_definitions = list()
+
+    for l in ddl.splitlines():
+        l = l.split()
+        try:
+            dtype = l[1].lower()
+            assert dtype.startswith(data_types)
+            nullable = 'NULL' if l[2].lower() == 'null' else 'NOT NULL'
+            column_definitions.append(
+                ColumnDefinition(l[0], dtype.upper(), nullable))
+        except AssertionError:
+            print(l)
+        except IndexError:
+            continue
+
+    return column_definitions
 
 
 def gen_connection_string(d, engine='postgresql'):
