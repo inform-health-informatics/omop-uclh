6354cf775fc8575d512c7dd7c3e39388c5d953a8
Steve Harris <doc@steveharris.me>
2018-11-01 21:43:21 +0000

    added CDM table name as abstract property

--- fakers/TwoD.py
+++ fakers/TwoD.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from abc import ABC, abstractmethod, abstractproperty
-from .faker import ConceptKeys  # named tuple of ids and names
+from utils.omop import ConceptKeys
 
 
 class TwoD(ABC):
@@ -14,15 +14,29 @@ class TwoD(ABC):
 
     def __init__(self, ts, colname=None):
 
-        self.ts = ts # 2d timeseries
+        self.ts = ts  # 2d timeseries
         self.colname = colname
-        self.colname = self._gen_colname() # to label data
+        self.colname = self._gen_colname()  # to label data
+        # - [ ] @ENHANCEMENT: (2018-11-01) check that table and concept
+        #   properties are legitimate by querying the database
 
     @abstractproperty
     def conceptkeys(self):
         """Should return a ConceptKeys tuple for every class
 
         concept_id should be mandated but shortname and FSN should be optional
+
+        Decorators:
+            abstractproperty
+        """
+        pass
+
+    @abstractproperty
+    def cdm_table(self):
+        """Target table in the OMOP CMD
+
+        Should return a 'key' corresponding to the table
+        Manage consistency through a class dictionary?
 
         Decorators:
             abstractproperty
@@ -46,6 +60,10 @@ class Lactate(TwoD):
     def conceptkeys(self):
         return ConceptKeys(3047181, 'lactate', 'Lactate [Moles/volume] in Blood')
 
+    @property
+    def cdm_table(self):
+        return 'measurement'
+
     # - [ ] @TODO: (2018-10-30) use mixins for these simulations
     def simulate(self, cadence=None):
         ts = self.ts
@@ -59,11 +77,14 @@ class HeartRate(TwoD):
     def conceptkeys(self):
         return ConceptKeys(4239408, 'hrate', 'Heart rate (observable entity)')
 
+    @property
+    def cdm_table(self):
+        return 'measurement'
+
     def simulate(self, cadence=None):
         ts = self.ts
         vals = [np.random.normal(90, 15) for i in ts]
         return pd.DataFrame.from_dict({'timestamp': ts, self.colname: vals})
-
 
 
 # - [ ] @TODO: (2018-10-30) convert to a test
